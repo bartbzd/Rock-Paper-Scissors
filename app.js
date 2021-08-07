e0b63c4b9438bcb1baa875f64600f181acef7a95
bartbzd <bbart318@gmail.com>
2021-08-06 21:48:43 -0500

    Clean up unneccesary comment usage

--- app.js
+++ app.js
@@ -4,13 +4,10 @@ function computerPlay (){
     return randomChoice[Math.floor(Math.random() * randomChoice.length)];
 }
 
-//Initial player and computer score
 let playerScore = 0
 let computerScore = 0
 
-//Check user selection versus computer selection, return winner
 function playRound(playerSelection, computerSelection) {
-    //Winning Conditions
     if (
         playerSelection === "rock" && computerSelection === "scissors" ||
         playerSelection === "scissors" && computerSelection === "paper" || 
@@ -19,7 +16,6 @@ function playRound(playerSelection, computerSelection) {
         return "You win!"
     }
 
-    //Losing Conditions
     if (
         playerSelection === "scissors" && computerSelection === "rock" ||
         playerSelection === "paper" && computerSelection === "scissors" ||
@@ -28,12 +24,10 @@ function playRound(playerSelection, computerSelection) {
         return "You lose!"
     }
 
-    //Tie Conditions
     if (playerSelection === computerSelection){
         return "It's a draw!"
     } 
-    
-    //Didn't enter rock, paper or scissors
+
     else {
         alert("Not Rock, Paper or Scissors, try again")
         console.clear()
@@ -41,14 +35,12 @@ function playRound(playerSelection, computerSelection) {
     }
 };
 
-//Play through game
 function game(){
     let computerSelection = computerPlay();
     let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
     console.log(playRound(playerSelection, computerSelection))
     score()
 
-//Play until score of 5
 function score(){
     if (playerScore < 5 && computerScore < 5){
         game()
@@ -65,7 +57,6 @@ function score(){
 }
 }
 
-//Resets game
 function resetGame() {
     playerScore = 0
     computerScore = 0
