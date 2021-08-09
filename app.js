33813aa39d8609d99695222b82fd7a1c387b3831
bartbzd <bbart318@gmail.com>
2021-08-08 23:29:38 -0500

    Remove score function to condense into game function

--- app.js
+++ app.js
@@ -1,65 +1,62 @@
 //Random selection by computer
-function computerPlay (){
-    const randomChoice = ["rock", "paper", "scissors"]
-    return randomChoice[Math.floor(Math.random() * randomChoice.length)];
+function computerPlay() {
+  const randomChoice = ["rock", "paper", "scissors"];
+  return randomChoice[Math.floor(Math.random() * randomChoice.length)];
 }
 
-let playerScore = 0
-let computerScore = 0
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
-    if (
-        playerSelection === "rock" && computerSelection === "scissors" ||
-        playerSelection === "scissors" && computerSelection === "paper" || 
-        playerSelection === "paper" && computerSelection === "rock") {
-        playerScore++
-        return "You win!"
-    }
+  if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")
+  ) {
+    playerScore++;
+    return "You win!";
+  }
 
-    if (
-        playerSelection === "scissors" && computerSelection === "rock" ||
-        playerSelection === "paper" && computerSelection === "scissors" ||
-        playerSelection === "rock" && computerSelection === "paper") {
-        computerScore++
-        return "You lose!"
-    }
+  if (
+    (playerSelection === "scissors" && computerSelection === "rock") ||
+    (playerSelection === "paper" && computerSelection === "scissors") ||
+    (playerSelection === "rock" && computerSelection === "paper")
+  ) {
+    computerScore++;
+    return "You lose!";
+  }
 
-    if (playerSelection === computerSelection){
-        return "It's a draw!"
-    } 
-
-    else {
-        alert("Not Rock, Paper or Scissors, try again")
-        console.clear()
-        resetGame()
-    }
-};
-
-function game(){
-    let computerSelection = computerPlay();
-    let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
-    console.log(playRound(playerSelection, computerSelection))
-    score()
-
-function score(){
-    if (playerScore < 5 && computerScore < 5){
-        game()
-    } else if (playerScore === 5) {
-        alert("YOU BEAT THE COMPUTER")
-        resetGame()
-    } else if (computerScore === 5){
-        alert("YOU LOST TO THE COMPUTER")
-        resetGame()
-    } else if (playerScore === computerScore){
-        alert("IT'S A DRAW")
-        resetGame()
-    }
+  if (playerSelection === computerSelection) {
+    return "It's a draw!";
+  } else {
+    alert("Not Rock, Paper or Scissors, try again");
+    console.clear();
+    resetGame();
+  }
 }
+
+function game() {
+  let computerSelection = computerPlay();
+  let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+  console.log(playRound(playerSelection, computerSelection));
+
+  if (playerScore < 5 && computerScore < 5) {
+    game();
+  } else if (playerScore === 5) {
+    alert("YOU BEAT THE COMPUTER");
+    resetGame();
+  } else if (computerScore === 5) {
+    alert("YOU LOST TO THE COMPUTER");
+    resetGame();
+  } else if (playerScore === computerScore) {
+    alert("IT'S A DRAW");
+    resetGame();
+  }
 }
 
 function resetGame() {
-    playerScore = 0
-    computerScore = 0
-    game()
+  playerScore = 0;
+  computerScore = 0;
+  game();
 }
 game();
