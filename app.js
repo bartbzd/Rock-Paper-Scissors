b12a7dae619cda13a30757e4b8403b772ef3b130
bartbzd <bbart318@gmail.com>
2021-09-02 20:01:36 -0500

    Remove five round logic

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 //Random selection by computer
 function computerPlay() {
-  const randomChoice = ["rock", "paper", "scissors"];
-  return randomChoice[Math.floor(Math.random() * randomChoice.length)];
+  const randomChoice = ["rock", "paper", "scissors"]
+  return randomChoice[Math.floor(Math.random() * randomChoice.length)]
 }
 
-let playerScore = 0;
-let computerScore = 0;
+let playerScore = 0
+let computerScore = 0
 
 function playRound(playerSelection, computerSelection) {
   if (
@@ -13,8 +13,8 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === "scissors" && computerSelection === "paper") ||
     (playerSelection === "paper" && computerSelection === "rock")
   ) {
-    playerScore++;
-    return "You win!";
+    playerScore++
+    return "You win!"
   }
 
   if (
@@ -22,41 +22,41 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === "paper" && computerSelection === "scissors") ||
     (playerSelection === "rock" && computerSelection === "paper")
   ) {
-    computerScore++;
-    return "You lose!";
+    computerScore++
+    return "You lose!"
   }
 
   if (playerSelection === computerSelection) {
-    return "It's a draw!";
+    return "It's a draw!"
   } else {
-    alert("Not Rock, Paper or Scissors, try again");
-    console.clear();
-    resetGame();
+    alert("Not Rock, Paper or Scissors, try again")
+    console.clear()
+    resetGame()
   }
 }
 
 function game() {
-  let computerSelection = computerPlay();
-  let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
-  console.log(playRound(playerSelection, computerSelection));
+  let computerSelection = computerPlay()
+  let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase()
+  console.log(playRound(playerSelection, computerSelection))
 
-  if (playerScore < 5 && computerScore < 5) {
-    game();
-  } else if (playerScore === 5) {
-    alert("YOU BEAT THE COMPUTER");
-    resetGame();
-  } else if (computerScore === 5) {
-    alert("YOU LOST TO THE COMPUTER");
-    resetGame();
-  } else if (playerScore === computerScore) {
-    alert("IT'S A DRAW");
-    resetGame();
-  }
+  // if (playerScore < 5 && computerScore < 5) {
+  //   game();
+  // } else if (playerScore === 5) {
+  //   alert("YOU BEAT THE COMPUTER");
+  //   resetGame();
+  // } else if (computerScore === 5) {
+  //   alert("YOU LOST TO THE COMPUTER");
+  //   resetGame();
+  // } else if (playerScore === computerScore) {
+  //   alert("IT'S A DRAW");
+  //   resetGame();
+  // }
 }
 
 function resetGame() {
-  playerScore = 0;
-  computerScore = 0;
-  game();
+  playerScore = 0
+  computerScore = 0
+  game()
 }
-game();
+game()
