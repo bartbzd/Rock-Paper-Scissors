160056b8c6e63ca624390d0e1530a3dcf0d777f5
bartbzd <bbart318@gmail.com>
2021-09-08 00:34:26 -0500

    Work and review the DOM to make all buttons work

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+const rock = document.getElementById("rock")
+const paper = document.getElementById("paper")
+const scissors = document.getElementById("scissors")
+const pScore = document.getElementById("pScoreNum")
+const cScore = document.getElementById("cScoreNum")
+const cSelect = document.getElementById("computerSelect")
+const gameInfo = document.getElementById("game-info")
+const round = document.getElementById("round")
+const reset = document.getElementById("game-reset")
+const cBtn = document.querySelector("#pcSelection")
+const pBtn = document.querySelector(".pSelection")
+
+rock.addEventListener("click", () => pClick("rock"))
+paper.addEventListener("click", () => pClick("paper"))
+scissors.addEventListener("click", () => pClick("scissors"))
+reset.addEventListener("click", () => resetGame())
 
 //Random selection by computer
 function computerPlay() {
@@ -7,6 +23,7 @@ function computerPlay() {
 
 let playerScore = 0
 let computerScore = 0
+let roundNum = 0
 let result = ""
 
 function playRound(playerSelection, computerSelection) {
@@ -15,8 +32,9 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === "scissors" && computerSelection === "paper") ||
     (playerSelection === "paper" && computerSelection === "rock")
   ) {
-    playerScore+= 1
-    return (result = "You win!")
+    playerScore++
+    roundNum++
+    result = "win"
   }
 
   if (
@@ -24,41 +42,69 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === "paper" && computerSelection === "scissors") ||
     (playerSelection === "rock" && computerSelection === "paper")
   ) {
-    computerScore+= 1
-    return (result = "You lose!")
+    computerScore++
+    roundNum++
+    result = "lose"
   }
 
   if (playerSelection === computerSelection) {
-    return (result = "It's a draw!")
-  } else {
-    alert("Not Rock, Paper or Scissors, try again")
-    console.clear()
-    resetGame()
+    roundNum++
+    result = "draw"
+  }
+}
+function winnerOrLoser() {
+  return playerScore === 5 || computerScore === 5
+}
+function pClick(playerSelection) {
+  gameInfo.style.fontSize = "35px"
+  if (winnerOrLoser()) {
+    return finalScore()
+  }
+  let computerSelection = computerPlay()
+  playRound(playerSelection, computerSelection)
+  console.log(playerSelection, computerSelection)
+  const cPick = `fa-hand-${computerSelection}`
+  cSelect.classList = `fas ${cPick} fa-5x`
+  updateScore()
+}
+function updateScore() {
+  if (result === "draw") {
+    gameInfo.textContent = "Draw!"
+    cBtn.style.border = "5px orange solid"
+  } else if (result === "win") {
+    gameInfo.textContent = "You win this round!"
+    cBtn.style.border = "5px red solid"
+  } else if (result === "lose") {
+    gameInfo.textContent = "You lose this round!"
+    cBtn.style.border = "5px green solid"
+  }
+
+  round.textContent = `Round: ${roundNum}`
+  pScore.textContent = `${playerScore}`
+  cScore.textContent = `${computerScore}`
+  if (winnerOrLoser()) {
+    finalScore()
   }
 }
 
-function game() {
-  let computerSelection = computerPlay()
-  let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase()
-  console.log(playRound(playerSelection, computerSelection))
-
-  // if (playerScore < 5 && computerScore < 5) {
-  //   game();
-  // } else if (playerScore === 5) {
-  //   alert("YOU BEAT THE COMPUTER");
-  //   resetGame();
-  // } else if (computerScore === 5) {
-  //   alert("YOU LOST TO THE COMPUTER");
-  //   resetGame();
-  // } else if (playerScore === computerScore) {
-  //   alert("IT'S A DRAW");
-  //   resetGame();
-  // }
+function finalScore() {
+  if (playerScore > computerScore) {
+    gameInfo.style.fontSize = "40px"
+    gameInfo.textContent = "Congratulations! You won!"
+    round.textContent = ""
+  } else {
+    gameInfo.style.fontSize = "40px"
+    gameInfo.textContent = "Uh-oh, you lost!"
+  }
 }
 
 function resetGame() {
   playerScore = 0
   computerScore = 0
-  game()
+  roundNum = 0
+  pScore.textContent = "0"
+  cScore.textContent = "0"
+  round.textContent = "Round: 0"
+  gameInfo.textContent = "FIGHT!"
+  gameInfo.style.fontSize = "55px"
 }
-game()
