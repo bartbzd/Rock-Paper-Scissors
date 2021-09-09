de28d2e99f45c569e16aa760f08954a3e7de147c
bartbzd <bbart318@gmail.com>
2021-09-08 21:21:26 -0500

    Reorganized code, added playerBtnColor

--- app.js
+++ app.js
@@ -1,30 +1,48 @@
-const rock = document.getElementById("rock")
-const paper = document.getElementById("paper")
-const scissors = document.getElementById("scissors")
-const pScore = document.getElementById("pScoreNum")
-const cScore = document.getElementById("cScoreNum")
-const cSelect = document.getElementById("computerSelect")
-const gameInfo = document.getElementById("game-info")
-const round = document.getElementById("round")
-const reset = document.getElementById("game-reset")
-const cBtn = document.querySelector("#pcSelection")
-const pBtn = document.querySelectorAll(".pSelection")
+let playerScore = 0
+let computerScore = 0
+let roundNum = 0
+let result = ""
 
+//DOM Selectors
+const rock = document.querySelector("#rock")
+const paper = document.querySelector("#paper")
+const scissors = document.querySelector("#scissors")
+const pScore = document.querySelector("#pScoreNum")
+const cScore = document.querySelector("#cScoreNum")
+const cSelect = document.querySelector("#computerSelect")
+const gameInfo = document.querySelector("#game-info")
+const round = document.querySelector("#round")
+const reset = document.querySelector("#game-reset")
+const cBtn = document.querySelector("#pcSelection")
+
+//DOM Listeners
 rock.addEventListener("click", () => pClick("rock"))
 paper.addEventListener("click", () => pClick("paper"))
 scissors.addEventListener("click", () => pClick("scissors"))
 reset.addEventListener("click", () => resetGame())
 
-//Random selection by computer
+//Computer Random Selection
 function computerPlay() {
   const randomChoice = ["rock", "paper", "scissors"]
   return randomChoice[Math.floor(Math.random() * randomChoice.length)]
 }
 
-let playerScore = 0
-let computerScore = 0
-let roundNum = 0
-let result = ""
+function pClick(playerSelection) {
+  gameInfo.style.fontSize = "35px"
+  resetPlayerColors()
+
+  if (endGame()) {
+    return finalScore()
+  }
+
+  let computerSelection = computerPlay()
+  playRound(playerSelection, computerSelection)
+  const cPick = `fa-hand-${computerSelection}`
+  if (cPick == "fa-hand-paper") {
+    cSelect.classList = `fas ${cPick} fa-5x rotate-45`
+  } else cSelect.classList = `fas ${cPick} fa-5x`
+  updateScore()
+}
 
 function playRound(playerSelection, computerSelection) {
   if (
@@ -35,8 +53,8 @@ function playRound(playerSelection, computerSelection) {
     playerScore++
     roundNum++
     result = "win"
+    cBtn.style.border = "5px #ff0021 solid"
   }
-
   if (
     (playerSelection === "scissors" && computerSelection === "rock") ||
     (playerSelection === "paper" && computerSelection === "scissors") ||
@@ -45,45 +63,65 @@ function playRound(playerSelection, computerSelection) {
     computerScore++
     roundNum++
     result = "lose"
+    cBtn.style.border = "5px #5cdb5c solid"
   }
-
   if (playerSelection === computerSelection) {
     roundNum++
     result = "draw"
+    cBtn.style.border = "5px #ffa500 solid"
+  }
+  playerBtnColor(playerSelection)
+}
+
+//Change player button border whether won or lost round
+function playerBtnColor(playerSelection) {
+  if (playerSelection === "rock" && result === "win") {
+    rock.style.border = "5px #5cdb5c solid"
+  }
+  if (playerSelection === "paper" && result === "win") {
+    paper.style.border = "5px #5cdb5c solid"
+  }
+  if (playerSelection === "scissors" && result === "win") {
+    scissors.style.border = "5px #5cdb5c solid"
+  }
+  if (playerSelection === "rock" && result === "lose") {
+    rock.style.border = "5px #ff0021 solid"
+  }
+  if (playerSelection === "paper" && result === "lose") {
+    paper.style.border = "5px #ff0021 solid"
+  }
+  if (playerSelection === "scissors" && result === "lose") {
+    scissors.style.border = "5px #ff0021 solid"
+  }
+  if (playerSelection === "rock" && result === "draw") {
+    rock.style.border = "5px #ffa500 solid"
+  }
+  if (playerSelection === "paper" && result === "draw") {
+    paper.style.border = "5px #ffa500 solid"
+  }
+  if (playerSelection === "scissors" && result === "draw") {
+    scissors.style.border = "5px #ffa500 solid"
   }
 }
-function winnerOrLoser() {
+
+function endGame() {
   return playerScore === 5 || computerScore === 5
 }
-function pClick(playerSelection) {
-  gameInfo.style.fontSize = "35px"
-  if (winnerOrLoser()) {
-    return finalScore()
-  }
-  let computerSelection = computerPlay()
-  playRound(playerSelection, computerSelection)
-  console.log(playerSelection, computerSelection)
-  const cPick = `fa-hand-${computerSelection}`
-  cSelect.classList = `fas ${cPick} fa-5x`
-  updateScore()
-}
+
 function updateScore() {
   if (result === "draw") {
     gameInfo.textContent = "Draw!"
-    cBtn.style.border = "5px orange solid"
   } else if (result === "win") {
     gameInfo.textContent = "You win this round!"
-    cBtn.style.border = "5px red solid"
   } else if (result === "lose") {
     gameInfo.textContent = "You lose this round!"
-    cBtn.style.border = "5px green solid"
   }
-
   round.textContent = `Round: ${roundNum}`
   pScore.textContent = `${playerScore}`
   cScore.textContent = `${computerScore}`
-  if (winnerOrLoser()) {
-    finalScore()
+
+  if (endGame()) {
+    return finalScore()
   }
 }
 
@@ -95,16 +133,26 @@ function finalScore() {
   } else {
     gameInfo.style.fontSize = "40px"
     gameInfo.textContent = "Uh-oh, you lost!"
+    round.textContent = ""
   }
 }
 
+function resetPlayerColors() {
+  rock.style.border = ""
+  paper.style.border = ""
+  scissors.style.border = ""
+}
+
 function resetGame() {
+  resetPlayerColors()
   playerScore = 0
   computerScore = 0
   roundNum = 0
   pScore.textContent = "0"
   cScore.textContent = "0"
-  round.textContent = "Round: 0"
+  round.textContent = ""
   gameInfo.textContent = "FIGHT!"
   gameInfo.style.fontSize = "55px"
+  cSelect.classList = ""
+  cBtn.style.border = ""
 }
